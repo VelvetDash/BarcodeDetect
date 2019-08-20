0b7f9cc5f9d0db7baf5c5768f7bdaffb880a4d1d
Velvet <soon.wenbin@dhs.sg>
2019-08-20 22:15:48 +0800

    .
    swipe up/down for back/front cam

---
+++ app/src/main/java/com/debug/barcodedetect/HistoryActivity.java
@@ -0,0 +1,141 @@
+package com.debug.barcodedetect;
+
+import android.content.ClipData;
+import android.content.ClipboardManager;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.util.Patterns;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class HistoryActivity extends AppCompatActivity {
+
+    private static final String TAG = "HistoryActivity";
+    private SharedPreferences prefs;
+    private String history;
+    private List<HashMap<String, String>> dataList;
+    private ListAdapter myAdapter;
+    private ListView myListView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_history);
+
+        prefs = getApplicationContext().getSharedPreferences("barcodedetect", MODE_PRIVATE);
+        history = prefs.getString("history",null);
+        myListView = (ListView) findViewById(R.id.history_list);
+        myListView.setEmptyView(findViewById(R.id.list_empty));
+
+        populateHistory();
+    }
+
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//        Log.d("optionsmenu","oncreate");
+//        getMenuInflater().inflate(R.menu.history_options, menu);
+//        return true;
+//    }
+//
+//    @Override
+//    public boolean onOptionsItemSelected(MenuItem item) {
+//        Log.d("optionsmenu","onselect");
+//        switch (item.getItemId()) {
+//            case R.id.clear_history:
+//                clearOrdersFromPref();
+//                break;
+//        }
+//        return true;
+//    }
+
+    private void populateHistory(){
+        String[] codes = getOrdersFromPref();
+        final List<HashMap<String, String>> dataList;
+        dataList = new ArrayList<>();
+        for (int i = 0; i < codes.length; i++) {
+            HashMap<String, String> element = new HashMap<>();
+            element.put("text1", codes[i]);
+//            element.put("text2", "");
+            dataList.add(element);
+        }
+        myAdapter = new SimpleAdapter(HistoryActivity.this, dataList, android.R.layout.simple_list_item_1, new String[]{"text1"}, new int[]{android.R.id.text1});
+        myListView.setAdapter(myAdapter);
+        myListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                // if url, open in browser
+
+                String potentialUrl = dataList.get(i).get("text1");
+                if (Patterns.WEB_URL.matcher(potentialUrl).matches()){
+                    Intent intent = new Intent(Intent.ACTION_VIEW);
+                    intent.setData(Uri.parse(potentialUrl));
+                    startActivity(intent);
+                }
+            }
+        });
+        myListView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> adapterView, View view, int i, long l) {
+                // copy to clipboard
+
+                ClipboardManager clipboardManager = (ClipboardManager)getSystemService(HistoryActivity.CLIPBOARD_SERVICE);
+                ClipData clipData;
+
+                String txtcopy = dataList.get(i).get("text1");
+                clipData = ClipData.newPlainText("text",txtcopy);
+                clipboardManager.setPrimaryClip(clipData);
+
+                return true;
+            }
+        });
+    }
+
+    private void clearOrdersFromPref(){
+        SharedPreferences prefs = getApplicationContext().getSharedPreferences("barcodedetect", MODE_PRIVATE);
+        prefs.edit().putString("history", null).apply();
+        myAdapter = new SimpleAdapter(HistoryActivity.this, new ArrayList<HashMap<String, String>>(), android.R.layout.simple_list_item_1, new String[]{"text1"}, new int[]{android.R.id.text1});
+        myListView.setAdapter(myAdapter);
+    }
+
+    private String[] getOrdersFromPref() {
+        SharedPreferences prefs = getApplicationContext().getSharedPreferences("barcodedetect", MODE_PRIVATE);
+        String json = prefs.getString("history", null);
+        Log.d(TAG,json);
+        String[] urls = new String[0];
+        if (json != null) {
+            try {
+                JSONArray allOrders = new JSONArray(json);
+                urls = new String[allOrders.length()];
+                for (int i = 0; i < allOrders.length(); i++) {
+                    JSONObject oneOrder = allOrders.getJSONObject(i);
+                    String code = oneOrder.getString("code");
+                    Log.i(TAG, code);
+                    urls[i] = code;
+                }
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+        Log.i(TAG, String.valueOf(urls.length));
+        return urls;
+    }
+
+}
---
+++ app/src/main/java/com/debug/barcodedetect/MainActivity.java
@@ -0,0 +1,158 @@
+package com.debug.barcodedetect;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.nfc.Tag;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.common.api.CommonStatusCodes;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+    private Button scanButton;
+    private int BARCODE_READER_REQUEST_CODE = 1;
+    private SharedPreferences prefs;
+
+    private float x1,x2,y1,y2;
+    static final int MIN_DISTANCE = 150;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        prefs = getApplicationContext().getSharedPreferences("barcodedetect", MODE_PRIVATE);
+
+        scanButton = (Button) findViewById(R.id.scan_barcode_button);
+
+        scanButton.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        startActivityForResult(
+                                new Intent(MainActivity.this, BarcodeCaptureActivity.class),
+                                BARCODE_READER_REQUEST_CODE
+                        );
+                    }
+                }
+        );
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        Log.d("optionsmenu","oncreate");
+        getMenuInflater().inflate(R.menu.history_options, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        Log.d("optionsmenu","onselect");
+        switch (item.getItemId()) {
+            case R.id.history:
+                Intent intent = new Intent(MainActivity.this, HistoryActivity.class);
+                MainActivity.this.startActivity(intent);
+                break;
+        }
+        return true;
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        switch(event.getAction())
+        {
+            case MotionEvent.ACTION_DOWN:
+                x1 = event.getX();
+                y1 = event.getY();
+                break;
+            case MotionEvent.ACTION_UP:
+                x2 = event.getX();
+                y2 = event.getY();
+                float deltaX = x2 - x1;
+                float deltaY = y2 - y1;
+                if (Math.abs(deltaX) > MIN_DISTANCE){
+                    Intent intent = new Intent(MainActivity.this, HistoryActivity.class);
+                    startActivity(intent);
+                    return true;
+                }
+                if (deltaY > MIN_DISTANCE){
+                    prefs.edit().putBoolean("frontcam", true).apply();
+                    startActivityForResult(
+                            new Intent(MainActivity.this, BarcodeCaptureActivity.class),
+                            BARCODE_READER_REQUEST_CODE
+                    );
+                    return true;
+                }
+                if (deltaY < -MIN_DISTANCE){
+                    prefs.edit().putBoolean("frontcam", false).apply();
+                    startActivityForResult(
+                            new Intent(MainActivity.this, BarcodeCaptureActivity.class),
+                            BARCODE_READER_REQUEST_CODE
+                    );
+                    return true;
+                }
+
+                break;
+        }
+        return super.onTouchEvent(event);
+    }
+
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        Log.i(TAG, "onActivityResult");
+        if (requestCode == BARCODE_READER_REQUEST_CODE) {
+            if (resultCode == CommonStatusCodes.SUCCESS) {
+                if (data != null) {
+                    String barcode_result = data.getExtras().getString(BarcodeCaptureActivity.BarcodeObject);
+
+                    Toast toast = Toast.makeText(getApplicationContext(), barcode_result, Toast.LENGTH_SHORT);
+                    toast.show();
+                    addCodeToHistory(barcode_result);
+                } else {
+                    Toast toast = Toast.makeText(getApplicationContext(),
+                            getString(R.string.no_barcode_captured),
+                            Toast.LENGTH_SHORT);
+                    toast.show();
+                    Log.i(TAG, getString(R.string.no_barcode_captured));
+                }
+            } else
+                Log.e(TAG, String.format(getString(R.string.barcode_error_format),
+                        CommonStatusCodes.getStatusCodeString(resultCode)));
+        } else
+            super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    private void addCodeToHistory(String s){
+        String json = prefs.getString("history", null);
+        JSONArray allOrders;
+        try {
+            JSONObject o = new JSONObject();
+            o.put("code",s);
+            if (json != null) {
+                allOrders = new JSONArray(json);
+            } else {
+                allOrders = new JSONArray();
+            }
+            allOrders = allOrders.put(o);
+            prefs.edit().putString("history", allOrders.toString()).apply();
+            Log.d(TAG, allOrders.toString());
+            Log.i(TAG, "registered "+s);
+        } catch (JSONException e){
+            e.printStackTrace();
+        }
+    }
+
+
+}
